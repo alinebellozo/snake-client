92ddc1ba13c20cb76c487edd123790121b43a910
Aline Bellozo <alinebellozo@gmail.com>
2023-03-24 03:13:43 +0000

    Refactoring the connect function using ES6 Shorthand syntax

---
+++ client.js
@@ -0,0 +1,18 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  // Node's net library (specifically, the createConnection function) to create an object named conn.
+  conn.setEncoding("utf8");
+
+  // The conn object that Node returned to you represents the connection that you have with the server.
+  return conn;
+};
+
+module.exports = { connect };
